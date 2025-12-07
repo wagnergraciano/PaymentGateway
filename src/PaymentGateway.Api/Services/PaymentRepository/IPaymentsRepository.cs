25618c2e232e08280022912cc1afda939acb473e
Wagner <wagner_graciano0101@hotmail.com>
2025-12-07 11:26:57 -0300

    Create interfaces for Repository and PaymentProcessor

---
+++ src/PaymentGateway.Api/Services/PaymentRepository/IPaymentsRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace PaymentGateway.Api.Services.PaymentRepository;
+
+public interface IPaymentsRepository
+{
+    IList<Payment> Payments;    
+    async void AddPaymentAsync(Payment payment, CancellationToken cancellationToken);
+    async Payment GetPaymentByIdAsync(Guid id, CancellationToken cancellationToken);
+}
